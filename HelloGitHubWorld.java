ab037db2b1116a9dff6b8d2977782c6eca579845
Kushagra Kulshrestha <kusagrakulshrestha@gmail.com>
2025-10-06 14:48:29 +0530

    print hello not github

--- HelloGitHubWorld.java
+++ HelloGitHubWorld.java
@@ -1,5 +1,5 @@
 public class HelloGitHubWorld {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub World");
+        System.out.println("hello not github");
     }
 }
